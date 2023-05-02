de7c40be24e0d9f4d366b2a7d4264c65cd5add3f
ernestassku <ernestassku@gmail.com>
2023-05-02 23:45:21 +0300

    Rewrite it to use Layer instead of window

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
 use std::{env, path::Path};
 
-use smithay_client_toolkit::{compositor::{CompositorState, CompositorHandler}, shell::{xdg::{XdgShell, window::{WindowDecorations, Window, WindowHandler}}, WaylandSurface, wlr_layer::{LayerShell, LayerShellHandler, Layer}}, shm::{Shm, slot::SlotPool, ShmHandler}, registry::{SimpleGlobal, RegistryState, ProvidesRegistryState}, output::{OutputState, OutputHandler}, delegate_compositor, delegate_output, delegate_shm, delegate_xdg_shell, delegate_xdg_window, delegate_simple, delegate_registry, registry_handlers, delegate_layer};
-use wayland_client::{Connection, globals::registry_queue_init, protocol::{wl_output, wl_shm, wl_surface}, QueueHandle, Dispatch};
+use smithay_client_toolkit::{
+    compositor::{CompositorHandler, CompositorState},
+    delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_shm,
+    delegate_simple, delegate_xdg_shell, delegate_xdg_window,
+    output::{OutputHandler, OutputState},
+    registry::{ProvidesRegistryState, RegistryState, SimpleGlobal},
+    registry_handlers,
+    seat::SeatState,
+    shell::{
+        wlr_layer::{Layer, LayerShell, LayerShellHandler, Anchor, LayerSurface},
+        xdg::{
+            window::{Window, WindowDecorations, WindowHandler},
+            XdgShell,
+        },
+        WaylandSurface,
+    },
+    shm::{slot::SlotPool, Shm, ShmHandler},
+};
+use wayland_client::{
+    globals::registry_queue_init,
+    protocol::{wl_output, wl_shm, wl_surface},
+    Connection, Dispatch, QueueHandle,
+};
 
 use wayland_protocols::wp::viewporter::client::{
     wp_viewport::{self, WpViewport},
     wp_viewporter::{self, WpViewporter},
 };
 
-
-struct App {
-}
+struct App {}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let connection = Connection::connect_to_env().unwrap();
     let (globals, mut event_queue) = registry_queue_init(&connection).unwrap();
-
+    
     let qh = event_queue.handle();
-
+    
     let compositor = CompositorState::bind(&globals, &qh).expect("wl_compositor is not available");
-    let xdg_shell = XdgShell::bind(&globals, &qh).expect("xdg shell is not available");
     let layer_shell = LayerShell::bind(&globals, &qh).expect("Layer shell is no available");
-    //TODO: using wl_shm it will render in a buffer. Look into how it can be rendered in GPU
     let shm = Shm::bind(&globals, &qh).expect("wl shm is not available");
-
+    
     let wp_viewporter = SimpleGlobal::<wp_viewporter::WpViewporter, 1>::bind(&globals, &qh)
-        .expect("wp_viewporter not available"); 
+    .expect("wp_viewporter not available");
 
-    let mut windows = Vec::new();
+    let mut windows: Vec<Window> = Vec::new();
+    let mut layers = Vec::new();
     let mut pool_size = 0;
     for path in env::args_os().skip(1) {
         let image = match image::open(&path) {
@@ -45,29 +62,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         pool_size += image.width() * image.height() * 4;
 
         let surface = compositor.create_surface(&qh);
-        let window = xdg_shell.create_window(surface.clone(), WindowDecorations::RequestServer, &qh);
-        // let layer = layer_shell.create_layer_surface(&qh, surface, Layer::Background, Some("Sample"), None);
+        let layer =
+            layer_shell.create_layer_surface(&qh, surface, Layer::Background, Some("Sample"), None);
+        layer.set_anchor(Anchor::TOP);
+
+        layer.set_size(image.width(), image.height());
+        layer.commit();
 
         
-        // TODO: look into window app ID.
-        window.set_app_id("io.github.aphrodite");
-        window.set_min_size(Some((256, 256)));
-        let path: &Path = path.as_os_str().as_ref();
-        window.set_title(path.components().last().unwrap().as_os_str().to_string_lossy());
-        
-        window.commit();
-        // layer.commit();
-
-        let viewport = wp_viewporter.get().expect("Required wp_viewporters").get_viewport(window.wl_surface(), &qh, ());
-
-        windows.push(ImageViewer {
+        layers.push(BackgroundLayer {
             width: image.width(),
             height: image.height(),
-            window,
-            viewport,
+            layer,
             image,
             first_configure: true,
-            damaged: true,
+            damaged: true
+            
         });
     }
 
@@ -77,16 +87,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         registry_state: RegistryState::new(&globals),
         output_state: OutputState::new(&globals, &qh),
         shm,
-        wp_viewporter,
         pool,
-        windows,
+        // windows,
+        layers
     };
 
     loop {
         event_queue.blocking_dispatch(&mut state).unwrap();
 
-        if state.windows.is_empty() {
-            println!("exiting example");
+        if state.layers.is_empty() {
             break;
         }
     }
@@ -96,13 +105,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 struct State {
     registry_state: RegistryState,
+    // seat_state: SeatState,
     output_state: OutputState,
     shm: Shm,
-    wp_viewporter: SimpleGlobal<WpViewporter, 1>,
 
+    // wp_viewporter: SimpleGlobal<WpViewporter, 1>,
     pool: SlotPool,
-    windows: Vec<ImageViewer>,
+    // windows: Vec<ImageViewer>,
+    layers: Vec<BackgroundLayer>,
 }
+
+struct BackgroundLayer {
+    layer: LayerSurface,
+    image: image::RgbaImage,
+    width: u32,
+    height: u32,
+    first_configure: bool,
+    damaged: bool,
+}
+
 struct ImageViewer {
     window: Window,
     image: image::RgbaImage,
@@ -131,6 +152,7 @@ impl CompositorHandler for State {
         _surface: &wl_surface::WlSurface,
         _time: u32,
     ) {
+        println!("Frame compositor handler");
         self.draw(conn, qh);
     }
 }
@@ -168,38 +190,6 @@ impl OutputHandler for State {
     }
 }
 
-impl WindowHandler for State {
-    fn request_close(&mut self, conn: &Connection, qh: &wayland_client::QueueHandle<Self>, window: &Window) {
-        self.windows.retain(|v| v.window != *window);
-    }
-
-    fn configure(
-        &mut self,
-        conn: &Connection,
-        qh: &wayland_client::QueueHandle<Self>,
-        window: &Window,
-        configure: smithay_client_toolkit::shell::xdg::window::WindowConfigure,
-        serial: u32,
-    ) {
-        for viewer in &mut self.windows {
-            if viewer.window != *window {
-                continue;
-            }
-            if let (Some(width), Some(height)) = configure.new_size {
-                viewer.width = width.get();
-                viewer.height = height.get();
-                viewer.viewport.set_destination(width.get() as _, height.get() as _);
-                if !viewer.first_configure {
-                    viewer.window.commit();
-                }
-            }
-            
-            viewer.first_configure = false;
-
-        }
-        self.draw(conn, qh);
-    }
-}
 
 impl ShmHandler for State {
     fn shm_state(&mut self) -> &mut Shm {
@@ -210,16 +200,24 @@ impl ShmHandler for State {
 impl State {
     pub fn draw(&mut self, _conn: &Connection, _qh: &QueueHandle<Self>) {
         // todo!()
-        for viewer in &mut self.windows {
+        for viewer in &mut self.layers {
             if viewer.first_configure || !viewer.damaged {
                 continue;
             }
-            let window = &viewer.window;
+            let window = &viewer.layer;
             let width = viewer.image.width();
             let height = viewer.image.height();
             let stride = width as i32 * 4;
 
-            let (buffer, canvas) = self.pool.create_buffer(width as i32, height as i32, stride, wl_shm::Format::Argb8888).expect("create buffer");
+            let (buffer, canvas) = self
+                .pool
+                .create_buffer(
+                    width as i32,
+                    height as i32,
+                    stride,
+                    wl_shm::Format::Argb8888,
+                )
+                .expect("create buffer");
 
             for (pixel, argb) in viewer.image.pixels().zip(canvas.chunks_exact_mut(4)) {
                 argb[3] = pixel.0[3];
@@ -228,10 +226,15 @@ impl State {
                 argb[0] = pixel.0[2];
             }
 
-            window.wl_surface().damage_buffer(0, 0, viewer.width as i32, viewer.height as i32);
+            window
+                .wl_surface()
+                .damage_buffer(0, 0, viewer.width as i32, viewer.height as i32);
             viewer.damaged = false;
 
-            viewer.viewport.set_source(0.0, 0.0, viewer.width as f64, viewer.height as f64);
+            window.wl_surface().frame(_qh, window.wl_surface().clone());
+            // viewer
+            //     .layer
+            //     .set_source(0.0, 0.0, viewer.width as f64, viewer.height as f64);
 
             buffer.attach_to(window.wl_surface()).unwrap();
             window.wl_surface().commit();
@@ -244,12 +247,12 @@ delegate_output!(State);
 delegate_shm!(State);
 
 delegate_xdg_shell!(State);
-delegate_xdg_window!(State);
+// delegate_xdg_window!(State);
+delegate_layer!(State);
 
 delegate_simple!(State, WpViewporter, 1);
 
 delegate_registry!(State);
-delegate_layer!(State);
 
 impl ProvidesRegistryState for State {
     fn registry(&mut self) -> &mut RegistryState {
@@ -259,11 +262,7 @@ impl ProvidesRegistryState for State {
     registry_handlers!(OutputState);
 }
 
-impl AsMut<SimpleGlobal<WpViewporter, 1>> for State {
-    fn as_mut(&mut self) -> &mut SimpleGlobal<WpViewporter, 1> {
-        &mut self.wp_viewporter
-    }
-}
+
 
 impl Dispatch<WpViewport, ()> for State {
     fn event(
@@ -280,8 +279,13 @@ impl Dispatch<WpViewport, ()> for State {
 }
 
 impl LayerShellHandler for State {
-    fn closed(&mut self, conn: &Connection, qh: &QueueHandle<Self>, layer: &smithay_client_toolkit::shell::wlr_layer::LayerSurface) {
-        todo!()
+    fn closed(
+        &mut self,
+        conn: &Connection,
+        qh: &QueueHandle<Self>,
+        layer: &smithay_client_toolkit::shell::wlr_layer::LayerSurface,
+    ) {
+        self.layers.retain(|v| v.layer != *layer);
     }
 
     fn configure(
@@ -292,7 +296,26 @@ impl LayerShellHandler for State {
         configure: smithay_client_toolkit::shell::wlr_layer::LayerSurfaceConfigure,
         serial: u32,
     ) {
-        todo!()
+        // todo!()
+        for l in &mut self.layers {
+            if l.layer != *layer { 
+                continue;
+            }
+
+            match configure.new_size {
+                (width, height) => {
+                    l.width = width;
+                    l.height = height;
+    
+                }
+                _ => {
+                    l.height = l.image.height();
+                    l.width = l.image.width();
+                },
+            }
+            l.first_configure = false;
+        }
+        self.draw(conn, qh);
     }
 }
 
@@ -301,3 +324,4 @@ impl Drop for ImageViewer {
         self.viewport.destroy()
     }
 }
+
