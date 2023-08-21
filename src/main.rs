3e072c3b6725fc281a0613d9c809bbe68c5672af
ernestassku <ernestassku@gmail.com>
2023-08-22 02:27:40 +0300

    Reset

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use EngineCore::connection::EngineEnvironment;
 use color_eyre::eyre::Result;
 
 mod EngineCore;
@@ -8,7 +7,6 @@ fn main() -> Result<()> {
     env_logger::init();
     color_eyre::install()?;
 
-    let engine = EngineCore::engine::Engine::connect(EngineCore::connection::ConnectionMode::Shell);
 
 
     Ok(())
