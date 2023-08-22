8cfbcea402d189862529382e6e2c334b601ac04b
ernestassku <ernestassku@gmail.com>
2023-08-23 00:02:45 +0300

    Green square

---
+++ src/AphroditeCore/engine.rs
@@ -0,0 +1,277 @@
+use raw_window_handle::{
+    HasRawDisplayHandle, HasRawWindowHandle, RawDisplayHandle, RawWindowHandle,
+};
+use smithay_client_toolkit::{
+    compositor::CompositorHandler,
+    delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_seat,
+    delegate_xdg_shell,
+    output::{OutputHandler, OutputState},
+    registry::{ProvidesRegistryState, RegistryHandler, RegistryState},
+    registry_handlers,
+    seat::{SeatHandler, SeatState},
+    shell::{
+        wlr_layer::{LayerShellHandler, LayerSurface},
+        WaylandSurface,
+    },
+};
+
+pub trait Engine {
+    // fn init()
+    fn process_frame(&self);
+}
+
+pub struct DisplayHandle(pub RawDisplayHandle, pub RawWindowHandle);
+
+unsafe impl HasRawDisplayHandle for DisplayHandle {
+    fn raw_display_handle(&self) -> RawDisplayHandle {
+        self.0
+    }
+}
+
+unsafe impl HasRawWindowHandle for DisplayHandle {
+    fn raw_window_handle(&self) -> RawWindowHandle {
+        self.1
+    }
+}
+
+pub struct EngineCore {
+    pub adapter: wgpu::Adapter,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub surface: Option<wgpu::Surface>,
+}
+
+impl EngineCore {
+    pub fn init_wgpu(display: Option<DisplayHandle>) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            dx12_shader_compiler: Default::default(),
+        });
+
+        let surface_owned = if let Some(handle) = display {
+            Some(unsafe { instance.create_surface(&handle).unwrap() })
+        } else {
+            None
+        };
+
+        let surface = surface_owned.as_ref();
+
+        let adapter =
+            pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptionsBase {
+                compatible_surface: surface,
+                ..Default::default()
+            }))
+            .expect("Failed to get adapter");
+
+        let (device, queue) = pollster::block_on(adapter.request_device(&Default::default(), None))
+            .expect("Failed to get device");
+
+        Self {
+            adapter,
+            device,
+            queue,
+            surface: surface_owned,
+        }
+    }
+}
+
+pub struct EngineShell {
+    pub registry_state: RegistryState,
+    pub output_state: OutputState,
+    pub seat_state: SeatState,
+    pub layer: LayerSurface,
+    pub core: EngineCore,
+    pub exit: bool,
+}
+
+struct EngineSHM {
+    core: EngineCore,
+}
+
+impl CompositorHandler for EngineShell {
+    fn scale_factor_changed(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        surface: &wayland_client::protocol::wl_surface::WlSurface,
+        new_factor: i32,
+    ) {
+        // todo!()
+    }
+
+    fn frame(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        surface: &wayland_client::protocol::wl_surface::WlSurface,
+        time: u32,
+    ) {
+        println!("Compositor frame");
+        // todo!()
+    }
+}
+
+impl OutputHandler for EngineShell {
+    fn output_state(&mut self) -> &mut smithay_client_toolkit::output::OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+    }
+
+    fn update_output(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+    }
+
+    fn output_destroyed(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+    }
+}
+
+impl SeatHandler for EngineShell {
+    fn seat_state(&mut self) -> &mut SeatState {
+        &mut self.seat_state
+    }
+
+    fn new_seat(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        seat: wayland_client::protocol::wl_seat::WlSeat,
+    ) {
+    }
+
+    fn new_capability(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        seat: wayland_client::protocol::wl_seat::WlSeat,
+        capability: smithay_client_toolkit::seat::Capability,
+    ) {
+    }
+
+    fn remove_capability(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        seat: wayland_client::protocol::wl_seat::WlSeat,
+        capability: smithay_client_toolkit::seat::Capability,
+    ) {
+    }
+
+    fn remove_seat(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        seat: wayland_client::protocol::wl_seat::WlSeat,
+    ) {
+    }
+}
+
+impl LayerShellHandler for EngineShell {
+    fn closed(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        layer: &LayerSurface,
+    ) {
+        // todo!()
+    }
+
+    fn configure(
+        &mut self,
+        conn: &wayland_client::Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        layer: &LayerSurface,
+        configure: smithay_client_toolkit::shell::wlr_layer::LayerSurfaceConfigure,
+        serial: u32,
+    ) {
+        println!("Configure from shell");
+
+        self.layer
+            .wl_surface()
+            .frame(qh, self.layer.wl_surface().clone());
+
+        let width = 500;
+        let height = 500;
+
+        let adapter = &self.core.adapter;
+        let surface = self.core.surface.as_ref().unwrap();
+        let device = &self.core.device;
+        let queue = &self.core.queue;
+
+        let cap = surface.get_capabilities(&adapter);
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: cap.formats[0],
+            view_formats: vec![cap.formats[0]],
+            alpha_mode: wgpu::CompositeAlphaMode::Auto,
+            width: width,
+            height: height,
+            present_mode: wgpu::PresentMode::Fifo,
+        };
+
+        surface.configure(&device, &surface_config);
+
+        let surface_texture = surface
+            .get_current_texture()
+            .expect("faield to acquire next swapchain texture");
+
+        let texture_view = surface_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut ecnoder = device.create_command_encoder(&Default::default());
+        {
+            let _renderpass = ecnoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &texture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
+        }
+
+
+        queue.submit(Some(ecnoder.finish()));
+        surface_texture.present();
+        self.layer.wl_surface().commit();
+        // self.process_frame();
+        // todo!()
+    }
+}
+
+impl ProvidesRegistryState for EngineShell {
+    fn registry(&mut self) -> &mut RegistryState {
+        &mut self.registry_state
+    }
+
+    registry_handlers!(OutputState);
+}
+
+delegate_compositor!(EngineShell);
+delegate_output!(EngineShell);
+delegate_seat!(EngineShell);
+
+delegate_xdg_shell!(EngineShell);
+delegate_layer!(EngineShell);
+
+delegate_registry!(EngineShell);
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,70 @@
+use AphroditeCore::engine::{EngineCore, DisplayHandle, EngineShell, self};
 use color_eyre::eyre::Result;
+use raw_window_handle::{WaylandDisplayHandle, RawDisplayHandle, WaylandWindowHandle, RawWindowHandle};
+use smithay_client_toolkit::{compositor::CompositorState, shell::{wlr_layer::LayerShell, WaylandSurface}, registry::RegistryState, output::OutputState, seat::SeatState};
+use wayland_client::{Connection, globals::registry_queue_init, QueueHandle};
+// use wayland_backend::sys::client::Backend;
+use wayland_client::Proxy;
+use smithay_client_toolkit::shell::wlr_layer::Layer;
+use winit::event;
 
-mod EngineCore;
+mod AphroditeCore;
 
 
 fn main() -> Result<()> {
     env_logger::init();
     color_eyre::install()?;
 
+    let conn = Connection::connect_to_env().unwrap();
+    let (globals, mut event_queue) = registry_queue_init(&conn).unwrap();
+    let qh: QueueHandle<AphroditeCore::engine::EngineShell> = event_queue.handle();
 
+
+    let compositor_state = 
+        CompositorState::bind(&globals, &qh).expect("wl_compositor not available");
+    let layer_shell = LayerShell::bind(&globals, &qh).unwrap();
+    let surface = compositor_state.create_surface(&qh);
+    let layer = layer_shell.create_layer_surface(&qh, surface, Layer::Top, Some("Ahprodite"), None);
+
+    //Temp
+    layer.set_size(500, 500);
+    layer.commit();
+    //////
+
+    let display = {
+        let mut handle = WaylandDisplayHandle::empty();
+        handle.display = conn.backend().display_ptr() as *mut _;
+        let display_handle = RawDisplayHandle::Wayland(handle);
+
+        let mut handle = WaylandWindowHandle::empty();
+        handle.surface = layer.wl_surface().id().as_ptr() as *mut _;
+        let layer_handle = RawWindowHandle::Wayland(handle);
+
+        Some(DisplayHandle(display_handle, layer_handle))
+    };
+    let engine_core = AphroditeCore::engine::EngineCore::init_wgpu(display);
+
+    let mut engine_shell = EngineShell {
+        core: engine_core,
+        layer: layer,
+        registry_state: RegistryState::new(&globals),
+        seat_state: SeatState::new(&globals, &qh),
+        output_state: OutputState::new(&globals, &qh),   
+        exit: false,
+    };
+    
+    loop {
+        let a = event_queue.blocking_dispatch(&mut engine_shell).unwrap();
+        println!("Event => {a:?}");
+
+        if engine_shell.exit {
+            break;
+        }
+
+    }
+
+    drop(engine_shell.core);
+    drop(engine_shell.layer);
 
     Ok(())
 }
